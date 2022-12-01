1fa6397247a4dd34ce06b5e7ac81da2ca566a99b
Scott Schumacker <scottschu97@gmail.com>
2022-12-01 12:23:19 -0500

    Updated comments

--- R/dimension.R
+++ R/dimension.R
@@ -1,5 +1,6 @@
 # Scott Schumacker
 # R Script to store the code for the dimension function
+# This function allows you to see what class the object is as well as the number of rows and columns
 
 dimension <- function(data){
   type <- class(data)
