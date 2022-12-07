6f4d58209f3ae0e29ef9d25e66a73cc796d0bbe1
Scott Schumacker <scottschu97@gmail.com>
2022-12-06 21:20:27 -0500

    Updated package with roxygen2

--- R/dimension.R
+++ R/dimension.R
@@ -1,6 +1,10 @@
-# Scott Schumacker
-# R Script to store the code for the dimension function
-# This function allows you to see what class the object is as well as the number of rows and columns
+#' Get the dimensions of a data set
+#'
+#' @param data a data set to use
+#' @return print statements about your data
+#' @examples
+#' dimension(iris)
+#' @export
 
 dimension <- function(data){
   type <- class(data)
